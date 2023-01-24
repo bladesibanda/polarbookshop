fb32a5d179cbe802cb57e1e30fc2abe828405690
Blessed Sibanda <bladesibanda@gmail.com>
2023-01-24 18:23:44 +0200

    refactor build-docker-images script

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,31 +1,24 @@
 #!/bin/sh
 
 chmod +x gradlew
+./gradlew bootJar
 
-rm -rf microservices/catalog-service/build/libs/*
-./gradlew :microservices:catalog-service:bootJar
 cd microservices/catalog-service
 docker rmi catalog-service
 docker build -t catalog-service .
 cd ../../
 
-rm -rf microservices/order-service/build/libs/*
-./gradlew :microservices:order-service:bootJar
 cd microservices/order-service
 docker rmi order-service
 docker build -t order-service .
 cd ../../
 
-rm -rf spring-cloud/edge-service/build/libs/*
-./gradlew :spring-cloud:edge-service:bootJar
 cd spring-cloud/edge-service
 docker rmi edge-service
 docker build -t edge-service .
 cd ../../
 
-rm -rf spring-cloud/config-service/build/libs/*
-cd spring-cloud/edge-service
-./gradlew :spring-cloud:config-service:bootJar
+cd spring-cloud/config-service
 docker rmi config-service
 docker build -t config-service .
 cd ../../
